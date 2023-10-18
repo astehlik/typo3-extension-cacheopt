72891d1963e618cbfe15603b697466e400bf0c41
Alexander Stehlik <alex@stehlik-online.de>
2023-10-18 09:58:57 +0200

    [TASK] Update ext_emconf.php files

--- Tests/Functional/Fixtures/Extensions/cacheopt_test/ext_emconf.php
+++ Tests/Functional/Fixtures/Extensions/cacheopt_test/ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Intera GmbH',
     'version' => '1.0.0',
     'constraints' => [
-        'depends' => ['typo3' => '9.5.0-9.5.99'],
+        'depends' => ['typo3' => '11.5.0-11.5.99'],
         'conflicts' => [],
         'suggests' => [],
     ],
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,4 +1,5 @@
 <?php
+
 /** @noinspection PhpMissingStrictTypesDeclarationInspection */
 
 /** @var string $_EXTKEY */
@@ -7,16 +8,19 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Cache optimizer',
     'description' => 'Optimizes automatic cache clearing.',
     'category' => 'be',
-    'version' => '10.0.0',
+    'version' => '11.0.0',
     'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',
     'clearcacheonload' => true,
     'author' => 'Alexander Stehlik',
-    'author_email' => 'astehlik.deleteme@intera.de',
+    'author_email' => 'alexander.stehlik.deleteme@gmail.com',
     'author_company' => 'Intera GmbH',
     'constraints' => [
-        'depends' => ['typo3' => '10.0.0-10.4.99'],
+        'depends' => [
+            'php' => '7.4.0-8.2.99',
+            'typo3' => '11.5.0-11.5.99',
+        ],
         'conflicts' => [],
         'suggests' => [],
     ],
